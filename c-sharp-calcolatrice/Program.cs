c25ec2bfdc9a98e4d1b9f12a6358ff925e4cea86
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-22 00:57:27 +0100

    get 2 number from user

--- c-sharp-calcolatrice/Program.cs
+++ c-sharp-calcolatrice/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-      
+        Console.WriteLine("Inserisci il primo numero");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Inserisci il secondo numero");
+        int y = Convert.ToInt32(Console.ReadLine());
     }
 }
