300b40ea29879015a7271f619f36b53ef4baa399
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-22 01:11:16 +0100

    asking to user for add new nr. handling with while

--- c-sharp-calcolatrice/Program.cs
+++ c-sharp-calcolatrice/Program.cs
@@ -3,11 +3,33 @@
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        List<int> numeri = new List<int>(); //conserviamo i numeri inseriti dall'user
+
         Console.WriteLine("Inserisci il primo numero");
         int x = Convert.ToInt32(Console.ReadLine());
+        
+        numeri.Add(x); //aggiungiamo x all'array
 
         Console.WriteLine("Inserisci il secondo numero");
         int y = Convert.ToInt32(Console.ReadLine());
+
+        numeri.Add(y);//aggiungiamo y all'array
+        Console.WriteLine("Vuoi inserire un altro numero? (y/n)");
+
+        string domanda = Console.ReadLine(); //chiediamo all'user se vuole inserire altri numeri
+
+
+        while (domanda == "y")
+        {
+            Console.WriteLine("Inserisci altro numero");
+            int nuovoNumero = Convert.ToInt32(Console.ReadLine());
+            numeri.Add(nuovoNumero);
+            Console.WriteLine("Vuoi inserire un altro numero? (y/n)");
+            domanda = Console.ReadLine();
+        }
+
+ 
+
     }
 }
